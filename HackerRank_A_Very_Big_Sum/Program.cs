6e0976efc4b4ae3580206964f1eb9820c4eed277
profesordavidvaldez <42122724+profesordavidvaldez@users.noreply.github.com>
2021-05-24 15:09:04 -0400

    Challenge completed

--- HackerRank_A_Very_Big_Sum/Program.cs
+++ HackerRank_A_Very_Big_Sum/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace HackerRank_A_Very_Big_Sum
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank_A_Very_Big_Sum
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+
+            long result = Result.aVeryBigSum(ar);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
---
+++ HackerRank_A_Very_Big_Sum/Result.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRank_A_Very_Big_Sum
+{
+    public class Result
+    {
+        /*
+         * Complete the 'aVeryBigSum' function below.
+         *
+         * The function is expected to return a LONG_INTEGER.
+         * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+        */
+
+        public static long aVeryBigSum(List<long> ar)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < ar.Count; i++)
+            {
+                sum += ar[i];
+            }
+
+            return sum;
+        }
+    }
+}
